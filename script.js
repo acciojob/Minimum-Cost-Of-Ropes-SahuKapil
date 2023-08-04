312627e55bcfb74a264757bbd4d488356771d474
Kapil Sahu <116225142+DevKapil-101@users.noreply.github.com>
2023-08-04 21:11:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
 function calculateMinCost() {
-  const inputElement = document.getElementById("rope-lengths");
-  const resultElement = document.getElementById("result");
-  
-  const ropeLengths = inputElement.value.split(",").map(str => parseInt(str.trim()));
-  const minCost = minimumCostOfRopes(ropeLengths);
-  
-  resultElement.innerText = "Minimum cost: " + minCost;
-}
+  // Get the rope lengths from the input field.
+  var ropeLengths = document.getElementById("rope-lengths").value;
+  var ropeLengthsArray = ropeLengths.split(",");
 
-function minimumCostOfRopes(ropes) {
-  // Use a priority queue (min heap) to efficiently find the minimum
-  const PriorityQueue = require('./priorityQueue'); // Assuming you have the PriorityQueue implementation
-
-  const pq = new PriorityQueue();
-  
-  for (const rope of ropes) {
-    pq.enqueue(rope);
+  // Create a min-heap to store the rope lengths.
+  var minHeap = new PriorityQueue();
+  for (var i = 0; i < ropeLengthsArray.length; i++) {
+    minHeap.enqueue(parseInt(ropeLengthsArray[i]));
   }
 
-  let totalCost = 0;
-  while (pq.size() > 1) {
-    const sum = pq.dequeue() + pq.dequeue();
-    totalCost += sum;
-    pq.enqueue(sum);
+  // Initialize the minimum cost to 0.
+  var minCost = 0;
+
+  // While there are more than two ropes in the min-heap.
+  while (minHeap.size() > 2) {
+    // Extract the two smallest ropes from the min-heap.
+    var rope1 = minHeap.dequeue();
+    var rope2 = minHeap.dequeue();
+
+    // Calculate the cost of connecting the two ropes.
+    var cost = rope1 + rope2;
+
+    // Add the cost to the minimum cost.
+    minCost += cost;
+
+    // Enqueue the new rope into the min-heap.
+    minHeap.enqueue(cost);
   }
 
-  // return totalCost;
-	console.log(totalCost);
+  // Get the remaining rope from the min-heap.
+  var rope = minHeap.dequeue();
+
+  // Add the cost of connecting the remaining rope to the minimum cost.
+  minCost += rope;
+
+  // Set the result text.
+  document.getElementById("result").innerHTML = minCost;
 }
